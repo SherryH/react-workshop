b57dc5494fb0bf67365e591b8c1ba81a6391a516
Sherry Hsu <sherry.hsu@orionhealth.com>
2018-01-29 09:11:40 +1300

    Completed the Testing exercises

--- subjects/Testing/exercise.js
+++ subjects/Testing/exercise.js
@@ -51,24 +51,63 @@ describe("when <Tabs> is rendered", () => {
     )
   })
 
-  it("renders the Brazil tab")
+  it("renders the Brazil tab", () => {
+    expect(tabs[1].innerText).toEqual(
+      FixtureData[1].label,
+      "Brazil tas was not rendered"
+    )
+  })
 
-  it("renders the Russia tab")
+  it("renders the Russia tab", () => {
+    expect(tabs[2].innerText).toEqual(
+      FixtureData[2].label,
+      "Russia tab was not rendered"
+    )
+  })
 
   // you may want to use the `borderFixture` variable
-  it("activates the first tab")
+  it("activates the first tab", () => {
+    // the activeTab has the same style as borderFixture
+    // console.log("attributes in tab", tabs[0])
+    expect(tabs[0].style.borderBottomColor).toEqual(
+      borderFixture.style.borderBottomColor,
+      "First tab not activated"
+    )
+  })
 
-  it("does not activate the second tab")
+  it("does not activate the second tab", () => {
+    expect(tabs[1].style.borderBottomColor).toNotEqual(
+      borderFixture.style.borderBottomColor,
+      "Second tab should not be activated"
+    )
+  })
 
   describe("after clicking the second tab", () => {
-    beforeEach(() => {
+    beforeEach(done => {
       // TODO: simulate a click on the second tab
+      Simulate.click(tabs[1])
+      done()
     })
 
-    it("activates the second tab")
+    it("activates the second tab", () => {
+      expect(tabs[1].style.borderBottomColor).toEqual(
+        borderFixture.style.borderBottomColor,
+        "Second tab not activated"
+      )
+    })
 
-    it("deactivates the first tab")
+    it("deactivates the first tab", () => {
+      expect(tabs[0].style.borderBottomColor).toNotEqual(
+        borderFixture.style.borderBottomColor,
+        "First tab should not be activated"
+      )
+    })
 
-    it("puts the correct content in the panel")
+    it("puts the correct content in the panel", () => {
+      expect(panel.innerText).toEqual(
+        FixtureData[1].content,
+        "Second Tab content is not displayed on panel"
+      )
+    })
   })
 })
