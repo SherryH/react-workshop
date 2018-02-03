0c4669390f9f20bf896a2525beff99755c095cab
SherryH <enjoy.wind7rain@gmail.com>
2018-02-03 16:03:54 +1300

    Context: hitting submit button submits the form

--- subjects/Context/exercise.js
+++ subjects/Context/exercise.js
@@ -20,21 +20,55 @@ import ReactDOM from "react-dom"
 import PropTypes from "prop-types"
 
 class Form extends React.Component {
+  static childContextTypes = {
+    handleSubmit: PropTypes.func,
+    handleInputChange: PropTypes.func
+  }
+  state = {
+    values: {}
+  }
+  getChildContext() {
+    console.log("onSubmit", this.props.onSubmit)
+    return {
+      handleSubmit: this.props.onSubmit,
+      handleInputChange: args => {
+        console.log("name value", args)
+      }
+    }
+  }
+
   render() {
     return <div>{this.props.children}</div>
   }
 }
 
 class SubmitButton extends React.Component {
+  static contextTypes = {
+    handleSubmit: PropTypes.func
+  }
+
   render() {
-    return <button>{this.props.children}</button>
+    return (
+      <button onClick={this.context.handleSubmit}>
+        {this.props.children}
+      </button>
+    )
   }
 }
 
 class TextInput extends React.Component {
+  static contextTypes = {
+    handleInputChange: PropTypes.func
+  }
   render() {
     return (
       <input
+        onChange={event =>
+          this.context.handleInputChange({
+            name: this.props.name,
+            value: event.target.value
+          })
+        }
         type="text"
         name={this.props.name}
         placeholder={this.props.placeholder}
